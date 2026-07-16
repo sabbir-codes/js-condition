62a8435f78d1711ac659f2f48ca41429a80e8dcf
sabbiraiml <sabbir.ah799@gmail.com>
2026-07-16 15:22:13 +0600

    initial commit

---
+++ one.js
@@ -0,0 +1,11 @@
+let price1 = 450;
+let price2 = 545;
+
+console.log(price1 == price2);
+console.log(price1 === price2);
+console.log(price1 != price2);
+console.log(price1 !== price2);
+console.log(price1 > price2);
+console.log(price1 < price2);
+console.log(price1 >= price2);
+console.log(price1 <= price2);
