a12f5e8cf5b4f60d9ecedbdeb9cfedd2c081f91d
sabbiraiml <sabbir.ah799@gmail.com>
2026-07-18 14:20:55 +0600

    condition practices

---
+++ positive_negative.js
@@ -0,0 +1,9 @@
+let number = 5;
+if (number > 0) {
+    console.log("This is Positive Number");
+
+} else if (number < 0) {
+    console.log("This is Negative Number");
+} else {
+    console.log("This is ZERO");
+}
---
+++ stock.js
@@ -0,0 +1,5 @@
+let stock = 0;
+
+if (stock === 0) {
+    console.log("Out of stock");
+}
