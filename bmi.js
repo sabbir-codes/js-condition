fc47043f0f0901fcba8c401b237734545443afbd
sabbiraiml <sabbir.ah799@gmail.com>
2026-07-23 10:48:59 +0600

    bmi system

--- bmi.js
+++ bmi.js
@@ -1,5 +1,11 @@
-let bmi = 14;
+let bmi = 30;
 
-if(bmi < 18.5){
-console.log("bmi", bmi, "Underweight");
+if (bmi < 18.5) {
+    console.log("bmi", bmi, "Underweight");
+} else if (bmi < 24.9) {
+    console.log("bmi", bmi, "Normal");
+} else if (bmi < 29.9) {
+    console.log("bmi", bmi, "Overweight");
+} else {
+    console.log("bmi", bmi, "Obese");
 }
