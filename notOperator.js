4655045f515f7e491976aa7a5474034eb67693d6
sabbiraiml <sabbir.ah799@gmail.com>
2026-07-23 12:05:19 +0600

    not operator

---
+++ notOperator.js
@@ -0,0 +1,7 @@
+let hasPermission = true;
+
+if(!hasPermission){
+    console.log("Access Allowed");
+}else{
+    console.log("Access Denied");
+}
--- ternary3.js
+++ ternary3.js
@@ -1,3 +1,3 @@
-let price = 545;
+let price = 4545;
 let result = (price > 1000) ? "Expensive" : "Affordable";
 console.log(result);
