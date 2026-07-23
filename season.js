145498c247762e6921a003f541a7058c27d4e050
sabbiraiml <sabbir.ah799@gmail.com>
2026-07-23 11:24:57 +0600

    season

--- season.js
+++ season.js
@@ -1,5 +1,13 @@
-let month = 2;
+let month = 10;
 
-if(month === 12 || month === 1 || month === 2){
+if (month === 12 || month === 1 || month === 2) {
     console.log("Winter");
+} else if (3 <= month && month <= 5) {
+    console.log("Summer");
+} else if (6 <= month && month <= 9) {
+    console.log("Monsoon");
+} else if (10 <= month && month <= 11) {
+    console.log("Autumn");
+} else {
+    console.log("Invalid month number");
 }
