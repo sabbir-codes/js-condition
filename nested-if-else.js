7dd82bc9ad8af613a79a9c1e4f37ebd7dc75205c
sabbiraiml <sabbir.ah799@gmail.com>
2026-07-23 11:29:55 +0600

    nested if-else

---
+++ nested-if-else.js
@@ -0,0 +1,13 @@
+let age = 18;
+let hasID = true;
+
+if (age >= 18){
+    console.log("Age is Okay");
+    if(hasID){
+        console.log("Eligible to vote");
+    }else{
+        console.log("But has no ID");
+    }
+}else{
+    console.log("Age Restricted");
+}
