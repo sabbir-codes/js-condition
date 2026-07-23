fb8644fe2329d91ae1b7a668012ebfdf017c6f02
sabbiraiml <sabbir.ah799@gmail.com>
2026-07-23 09:17:25 +0600

    leap year

--- leapYear.js
+++ leapYear.js
@@ -1,7 +1,7 @@
-let year = 2000;
+let year = 2004;
 
-if((year % 4 === 0 && year % 100 !== 0)){
-    console.log("Leap year");
+if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0){
+    console.log(year, "Leap year");
 }else{
     console.log(year, "Not Leap Year");
 }
