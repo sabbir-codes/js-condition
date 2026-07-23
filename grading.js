d0786e958cf6c0b334ec9ac4bfe663ec91c7e7f0
sabbiraiml <sabbir.ah799@gmail.com>
2026-07-23 09:45:11 +0600

    grading system

---
+++ grading.js
@@ -0,0 +1,12 @@
+let mark = 45;
+if (mark >= 80){
+    console.log(mark, "A+");
+}else if(mark >= 70){
+    console.log(mark, "A");
+}else if(mark >= 60){
+    console.log(mark, "B");
+}else if(mark >= 50){
+    console.log(mark, "C");
+}else{
+    console.log(mark, "F");
+}
